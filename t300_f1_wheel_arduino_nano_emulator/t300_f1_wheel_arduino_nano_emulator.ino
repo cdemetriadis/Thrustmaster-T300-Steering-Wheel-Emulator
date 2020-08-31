fee9f6a5476d2e8ce8ad4196d5a2b47d1e91541b
Constantinos Demetriadis <cdemetriadis@gmail.com>
2020-08-31 20:03:26 +0300

    minor tweaks

--- t300_f1_wheel_arduino_nano_emulator/t300_f1_wheel_arduino_nano_emulator.ino
+++ t300_f1_wheel_arduino_nano_emulator/t300_f1_wheel_arduino_nano_emulator.ino
@@ -1,27 +1,27 @@
-#define   DEBUG true
-#define   DEBUG_WHEEL false
+#define     DEBUG true
+#define     DEBUG_WHEEL false
 
-int       foundColumn = 0; // Active column
-int       keyValue = 0;
-int       debounce = 300; // set this to the lowest value that gives the best result
-byte      wheelState[8]; // local push-buttons state saved here
-volatile  byte pos;
+int         foundColumn = 0; // Active column
+int         keyValue = 0;
+int         debounce = 300; // Set this to the lowest value that gives the best result
+byte        wheelState[8]; // local push-buttons state saved here
+volatile    byte pos;
 
 // Setup Button Grid
-int     rowPin[] = {3, 4, 5, 6, 7}; // Set pins for rows
+int     rowPin[] = {4, 5, 6, 7, 8, 9}; // Set pins for rows
 int     colPin[] = {A0, A1, A2, A3}; // Set pins for columns
 int     rowSize = sizeof(rowPin)/sizeof(rowPin[0]);
 int     colSize = sizeof(colPin)/sizeof(colPin[0]);
 
-//
-//      Cols  |  0             1             2             3
-// Rows Pins  |  A0 (14)       A1 (15)       A2 (16)       A3 (17)
-// -------------------------------------------------------------------
-// 0    3     |  42 Triangle   45 Square     48 Cross      51 Circle
-// 1    4     |  56 L1         60 L2         64 R2         68 R1
-// 2    5     |  70 Share      75 PS         80 Options    85 -
-// 3    6     |  84 Up         90 Down       96 Left       102 Right
-// 4    7     |  98 -          105 -         112 L3        119 R3
+//      Cols  |  0              1               2             3
+// Rows Pins  |  A0 (14)        A1 (15)         A2 (16)       A3 (17)
+// -------------------------------------------------------------------------
+// 0    4     |  185 Triangle   205 Square      226 Cross     248 Circle
+// 1    5     |  204 L1         225 L2          247 R2        270 R1
+// 2    6     |  224 Share      246 PS          269 Options   293 -
+// 3    7     |  245 Up         268 Down        292 Left      317 Right
+// 4    8     |  267 Enc Up     291 Enc Left    316 -         342 Enc L3
+// 5    9     |  290 Enc Down   315 Enc Right   341 -         368 Enc R3
 
 void setup(){
   
@@ -34,7 +34,7 @@ void setup(){
   wheelState[6] = B01100000;
   wheelState[7] = B01000000;
   
-  Serial.begin(9600);    // Arduino debug console
+  Serial.begin(115200);    // Arduino debug console
   pinMode(MISO, OUTPUT); // Arduino is a slave device
   SPCR |= _BV(SPE);      // Enables the SPI when 1
   SPCR |= _BV(SPIE);     // Enables the SPI interrupt when 1
@@ -108,125 +108,155 @@ void loop() {
   // Set the button action based on the keyValue
   switch (keyValue) {
   
-    case 167: // Triangle
+    case 185: // Triangle
       wheelState[0] = wheelState[0] & B10111111;
       #if DEBUG
         Serial.print("Button: Triangle ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 186: // Square
+    case 205: // Square
       wheelState[0] = wheelState[0] & B01111111;
       #if DEBUG
         Serial.print("Button: Square ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 206: // Cross
+    case 226: // Cross
       wheelState[1] = wheelState[1] & B11111101;
       #if DEBUG
         Serial.print("Button: Cross ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 227: // Circle
+    case 248: // Circle
       wheelState[0] = wheelState[0] & B11111110;
       #if DEBUG
         Serial.print("Button: Circle ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 185: // L1
+    case 204: // L1
       wheelState[0] = wheelState[0] & B11101111;
       #if DEBUG
         Serial.print("Button: L1 ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 205: // L2
+    case 225: // L2
       wheelState[1] = wheelState[1] & B11011111;
       #if DEBUG
         Serial.print("Button: L2 ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 226: // R1
+    case 247: // R1
       wheelState[0] = wheelState[0] & B11011111;
       #if DEBUG
         Serial.print("Button: R1 ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 248: // R2
+    case 270: // R2
       wheelState[1] = wheelState[1] & B11101111;
       #if DEBUG
         Serial.print("Button: R2 ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 204: // Share
+    case 224: // Share
       wheelState[1] = wheelState[1] & B11111011;
       #if DEBUG
         Serial.print("Button: Share ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 225: // PS
+    case 246: // PS
       wheelState[1] = wheelState[1] & B11111110;
       #if DEBUG
         Serial.print("Button: PS ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 247: // Options
+    case 269: // Options
       wheelState[1] = wheelState[1] & B11110111;
       #if DEBUG
         Serial.print("Button: Options ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 224: // Up
+    case 245: // Up
       wheelState[2] = wheelState[2] & B11101111;
       #if DEBUG
         Serial.print("Button: Up ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 246: // Down
+    case 268: // Down
       wheelState[2] = wheelState[2] & B11111101;
       #if DEBUG
         Serial.print("Button: Down ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 269: // Left
+    case 292: // Left
       wheelState[2] = wheelState[2] & B11110111;
       #if DEBUG
         Serial.print("Button: Left ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 293: // Right
+    case 317: // Right
       wheelState[2] = wheelState[2] & B11111011;
       #if DEBUG
         Serial.print("Button: Right ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
+
+
     
-    case 292: // L3
+    case 267: // Encoder Up
+      wheelState[2] = wheelState[2] & B11101111;
+      #if DEBUG
+        Serial.print("Encoder: Up ("); Serial.print(keyValue); Serial.println(") ");
+      #endif
+      break;
+    
+    case 290: // Encoder Down
+      wheelState[2] = wheelState[2] & B11111101;
+      #if DEBUG
+        Serial.print("Encoder: Down ("); Serial.print(keyValue); Serial.println(") ");
+      #endif
+      break;
+    
+    case 291: // Encoder Left
+      wheelState[2] = wheelState[2] & B11110111;
+      #if DEBUG
+        Serial.print("Encoder: Left ("); Serial.print(keyValue); Serial.println(") ");
+      #endif
+      break;
+    
+    case 315: // Encoder Right
+      wheelState[2] = wheelState[2] & B11111011;
+      #if DEBUG
+        Serial.print("Encoder: Right ("); Serial.print(keyValue); Serial.println(") ");
+      #endif
+      break;
+    
+    case 342: // Encoder L3
       wheelState[1] = wheelState[1] & B10111111;
       #if DEBUG
         Serial.print("Button: L3 ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
     
-    case 317: // R3
+    case 368: // Encoder R3
       wheelState[1] = wheelState[1] & B01111111;
       #if DEBUG
         Serial.print("Button: R3 ("); Serial.print(keyValue); Serial.println(") ");
       #endif
       break;
-    
+        
     default: // Reset if nothing is pressed
       wheelState[0] = B11001111; // F1 wheel specific, and 5 Button
       wheelState[1] = B11111111; // 8 Buttons
